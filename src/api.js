6951d31cadb1f6c74edfa2dc789cd1b778e715a8
csobr <siham.hadi@live.se>
2020-07-20 23:30:11 +0200

    Set location, store values

--- src/api.js
+++ src/api.js
@@ -2,37 +2,66 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 const Data = () => {
     const [data, setData] = useState([])
-    const [location, setLocation] = useState([])
+    const url =
+        'http://api.aladhan.com/v1/timingsByCity?&city=gislaved&country=sweden&method=8'
+
+    const [city, setCity] = useState(undefined)
+    const [country, setCountry] = useState(undefined)
     const [isLoading, setIsLoading] = useState(false)
     const [isError, setIsError] = useState(false)
-    navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
+    const handleSubmit = (e) => {
+        localStorage.setItem('City', city)
+        localStorage.setItem('Country', country)
+        e.preventDefault()
+    }
 
-        setLocation([longitude, latitude])
-    })
+    const params = new URLSearchParams(url)
+    params.set('city', city)
+    params.set('country', country)
+    // console.log(params.toString())
+
     useEffect(() => {
         const fetchData = async () => {
             setIsError(false)
             setIsLoading(true)
-            try {
-                const result = await axios({
-                    method: 'get',
-                    url:
-                        'http://api.aladhan.com/v1/timingsByCity?city=Gislaved&country=Sweden&method=8',
-                })
-                setData(result.data.data)
-            } catch (error) {
-                setIsError(true)
+            if (localStorage['Data']) {
+                setData(JSON.parse(localStorage.getItem('Data')))
+                setIsLoading(false)
+            } else {
+                alert('request')
+                try {
+                    const result = await axios({
+                        method: 'get',
+                        url: url,
+                    })
+                    localStorage.setItem(
+                        'Data',
+                        JSON.stringify(result.data.data)
+                    )
+
+                    setData(result.data.data)
+                } catch (error) {
+                    setIsError(true)
+                }
+                setIsLoading(false)
             }
-            setIsLoading(false)
         }
+        const storedValues = () => {
+            if (localStorage[('City', 'Country', 'Data')]) {
+                setCity(localStorage.getItem('City'))
+                setCountry(localStorage.getItem('Country'))
+            } else {
+                console.log('LS empty')
+            }
+        }
+        storedValues()
         fetchData()
     }, [])
 
     const timings = { ...data.timings }
     return (
         <>
+            <h1>City</h1>
             {isError && <div> Something went wrong</div>}
             {isLoading ? (
                 <div>Loading...</div>
@@ -45,10 +74,25 @@ const Data = () => {
                         <li>{timings.Maghrib}</li>
                         <li>{timings.Isha}</li>
                     </ul>
-                    <p>{location[0]}&deg;</p>
-                    <p>{location[1]}&deg;</p>
                 </div>
             )}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="City">
+                    <input
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    ></input>
+                </label>
+                <label htmlFor="Country">
+                    <input
+                        type="text"
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                    ></input>
+                </label>
+                <input type="submit" value="Submit"></input>
+            </form>
         </>
     )
 }
