a5ef14f15d397cd1f99b0439799191968ef66dd3
csobr <siham.hadi@live.se>
2020-08-07 21:54:27 +0200

    app updates for dist

---
+++ public/main.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const electron = require('electron')
+const isDev = require('electron-is-dev')
+const SalatTray = require('../src/tray')
+
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+let tray
+let win
+const iconPath = path.join(__dirname, `../assets/icon.png`)
+app.on('ready', () => {
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        height: 250,
+        width: 200,
+        frame: false,
+        resizable: false,
+        show: false,
+    })
+    const startURL = isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, 'index.html')}`
+    win.loadURL(startURL)
+    win.on('blur', () => {
+        win.hide()
+    })
+    tray = new SalatTray(iconPath, win)
+})
---
+++ src/tray.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+
+const { app, Tray, Menu } = electron
+
+class SalahTray extends Tray {
+    constructor(iconPath, win) {
+        super(iconPath)
+
+        this.win = win
+
+        this.setToolTip('Salat Times')
+        this.on('click', this.onClick.bind(this))
+        this.on('right-click', this.onRightClick.bind(this))
+    }
+    onClick(event, bounds) {
+        const { x, y } = bounds
+        const { height, width } = this.win.getBounds()
+        if (this.win.isVisible()) {
+            this.win.hide()
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height
+            this.win.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height,
+                width,
+            })
+            this.win.show()
+        }
+    }
+    onRightClick() {
+        const exitMenu = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+            },
+        ])
+        this.popUpContextMenu(exitMenu)
+    }
+}
+module.exports = SalahTray
